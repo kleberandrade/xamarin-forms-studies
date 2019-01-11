72167ef480452b646303ab81f202d348f4d4d9ca
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-01-11 09:26:07 -0200

    Projeto de perfil básico concluído

---
+++ Projeto04/BasicDashboardApp/BasicDashboardApp/BasicDashboardApp/Master/MenuPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BasicDashboardApp.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuPage : MasterDetailPage
+	{
+		public MenuPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoProfile1Page(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Profile1Page());
+            IsPresented = false;
+        }
+
+        private void GoAboutPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.AboutPage());
+            IsPresented = false;
+        }
+    }
+}
