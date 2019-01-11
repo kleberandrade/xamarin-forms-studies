abb9e230ea1e978aafd4241747ee783d5e78c543
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-01-11 08:09:23 -0200

    Projeto aprendendo sobre Layout concluído

---
+++ Projeto03/LayoutApp/LayoutApp/LayoutApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using LayoutApp.Absolute;
+using LayoutApp.Grid;
+using LayoutApp.Relative;
+using LayoutApp.Scroll;
+using LayoutApp.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoAbsolutePage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AbsolutePage());
+        }
+
+        private void GoGridPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GridPage());
+        }
+
+        private void GoRelativePage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RelativePage());
+        }
+
+        private void GoStackPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StackPage());
+        }
+
+        private void GoScrollPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ScrollPage());
+        }
+    }
+}
